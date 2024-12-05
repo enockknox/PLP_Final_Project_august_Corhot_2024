1f5e9a3fdd24befaa3ddb46ce99b34818de4f479
enockknox <knoxenock@gmail.com>
2024-12-05 16:26:41 +0300

    progress

--- penge/app/Http/Controllers/Api/AuthController.php
+++ penge/app/Http/Controllers/Api/AuthController.php
@@ -4,22 +4,60 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
 
 class AuthController extends Controller
 {
-    public function register() {
+    protected AuthService $authSerice;
+
+    public function __construct(AuthService $authService) {
+        $this->authService = $authService;
+    }
+    public function register(Request $request) : Response {
         // validate request
         $request-validate([
             'name' => 'required|string|max;255',
             "email" => 'required|email|max:255|unique:users,email',
             'password' => 'required|min:6|max:255'
-        ])
+        ]);
         // create user
-
+      $user = $this->authService->register($request);
         // create access token
+        $token = $user=>createToken('auth')=>plainTextToken;
 
         // return
+        return response([
+            'message' => 'app.registration_success',
+            'results' => {
+                'user' => new UserResource($user),
+                'token' => $token
+            }
+        ], 201);
 
     }
     
 }
+
+    public function login(Request $request) : Response {
+        // validate request
+        $request-validate([
+            "email" => 'required|email|max:255',
+            'password' => 'required|min:6|max:255'
+        ]);
+        // login user
+      $user = $this->authService->login($request);
+        // create access token
+        $token = $user=>createToken('auth')=>plainTextToken;
+
+        // return
+        return response([
+            'message' => 'app.registration_success',
+            'results' => {
+                'user' => new UserResource($user),
+                'token' => $token
+            }
+        ], 200);
+
+    }
+    
